d8ca5e951d4ee103a88baee36754ff337ad3ca2e
suha <gusuhawoo@gmail.com>
2023-12-30 21:23:45 +0900

    Code refactoring

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,20 +2,22 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  // const [x, setX] = useState(0);
+  // const [y, setY] = useState(0);
 
   return (
     <div
       className="container"
-      onPointerMove={(event) => {
-        setX(event.clientX);
-        setY(event.clientY);
+      onPointerMove={(e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+        // setX(e.clientX);
+        // setY(e.clientY);
       }}
     >
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px,${y}px)` }}
+        style={{ transform: `translate(${position.x}px,${position.y}px)` }}
       />
     </div>
   );
