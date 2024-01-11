d7598713d534390e4d0c7d143d1159fb6de06810
suha <gusuhawoo@gmail.com>
2024-01-11 09:29:08 +0900

    Add AppWrap

---
+++ src/AppWrap.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function AppWrap() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <header style={{ backgroundColor: 'yellow' }}>
+      <Avatar
+        image="https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?q=80&w=2707&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        name="Bob"
+        size={200}
+      />
+    </header>
+  );
+}
+
+function Avatar({ image, name, size }) {
+  return (
+    <div>
+      <img
+        src={image}
+        alt={`${name}`}
+        width={size}
+        height={size}
+        style={{ borderRadius: '50%' }}
+      />
+    </div>
+  );
+}
