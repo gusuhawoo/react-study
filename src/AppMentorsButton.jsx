f129eef91f09bf0963a52d460b110b799adca326
suha <gusuhawoo@gmail.com>
2024-01-18 08:32:46 +0900

    refactor: useMemo and useCallback to improve performance

--- src/AppMentorsButton.jsx
+++ src/AppMentorsButton.jsx
@@ -1,25 +1,25 @@
-import React, { useReducer } from 'react';
+import React, { memo, useCallback, useMemo, useReducer } from 'react';
 import personReducer from './reducer/person-reducer';
 
 export default function AppMentorsButton() {
   const [person, dispatch] = useReducer(personReducer, initialPerson);
 
-  const handleUpdate = () => {
+  const handleUpdate = useCallback(() => {
     const prev = prompt(`Whose name do you want to change?`);
     const current = prompt(`what's your mentor's name?`);
     dispatch({ type: 'updated', prev, current });
-  };
+  }, []);
 
-  const handleAdd = () => {
+  const handleAdd = useCallback(() => {
     const name = prompt(`what's your mentor's name?`);
     const title = prompt(`what's your mentor's title?`);
     dispatch({ type: 'added', name, title });
-  };
+  }, []);
 
-  const handleDelete = () => {
+  const handleDelete = useCallback(() => {
     const name = prompt(`Who do you want to delete?`);
     dispatch({ type: 'deleted', name });
-  };
+  }, []);
 
   return (
     <div>
@@ -41,8 +41,9 @@ export default function AppMentorsButton() {
   );
 }
 
-function Button({ text, onClick }) {
+const Button = memo(({ text, onClick }) => {
   console.log('Button', text, 're-rendering');
+  const result = useMemo(() => calculateSomething(), []);
 
   return (
     <button
@@ -54,9 +55,16 @@ function Button({ text, onClick }) {
         margin: '0.4rem',
       }}
     >
-      {text}
+      {`${text} ${result}`}
     </button>
   );
+});
+
+function calculateSomething() {
+  for (let i = 0; i < 10000; i++) {
+    console.log('☺️');
+  }
+  return 10;
 }
 
 const initialPerson = {
