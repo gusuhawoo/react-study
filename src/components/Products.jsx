87b089b9a5936f2880ee78f2eea983c5f096a4f9
suha <gusuhawoo@gmail.com>
2024-01-20 11:10:44 +0900

    Add : Loading & Error

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     // When mounted.
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => setProducts(data))
+      .catch((e) => setError('Error!'))
+      .finally(() => setLoading(false));
     // Network communication when certain values change.
     return () => {
       console.log('checked');
@@ -20,6 +26,9 @@ export default function Products() {
   // fetch('data/products.json')
   //   .then((res) => res.json())
   //   .then((data) => setProducts(data));
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <>
